5ec37ce4d493c864e789fbfaeb6a2d32032966eb
Artem Zyablitskiy <a.zyablitskiy@gmail.com>
2015-04-10 15:58:08 +0300

    init

---
+++ Smscua.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zyablitskiy
+ * Date: 10.04.2015
+ * Time: 15:54
+ */
+namespace samson\smscua;
+
+use samson\core\iModuleCompressable;
+
+/* Main class for sending messages  via eSputnik service */
+class eSputnik extends \samson\core\Service implements iModuleCompressable
+{
+
+}
